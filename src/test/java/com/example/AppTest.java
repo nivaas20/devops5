a874c292444350bef1eaa63e8db3f4885120eafd
nivaas20 <nivaas204@gmail.com>
2026-05-14 22:03:33 +0530

    first commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testadd() {
+
+        App app = new App();
+
+        int result = app.fact(4);
+
+        System.out.println("Running :" + result);
+
+        Assert.assertEquals(24, result);
+    }
+}
