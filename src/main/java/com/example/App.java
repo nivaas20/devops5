d7e01cd0f93721306d0101b11e44820600bc498a
nivaas20 <nivaas204@gmail.com>
2026-05-15 10:08:01 +0530

    Fixed factorial method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,21 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public int fact(int n) {
+
+        if(n == 1 || n == 0)
+            return 1;
+        else
+            return fact(n - 1) * n;
+    }
+
+    public static void main(String[] args) {
+
+        App obj = new App();
+
+        int res = obj.fact(4);
+
+        System.out.println("The Result=" + res);
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 public class AppTest {
 
     @Test
-    public void testadd() {
+    public void testFact() {
 
         App app = new App();
 
         int result = app.fact(4);
 
-        System.out.println("Running :" + result);
+        System.out.println("Running: " + result);
 
         Assert.assertEquals(24, result);
     }
